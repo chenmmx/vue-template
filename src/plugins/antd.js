7c36b951930f9acf330b0a66e8282595fb13ff6f
chenmmx <1107204891@qq.com>
2020-06-01 15:01:21 +0800

    feat: add charts example and modify some styles

--- src/plugins/antd.js
+++ src/plugins/antd.js
@@ -1,6 +1,5 @@
-import Vue from "vue";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/antd.less";
 import "@/styles/default.less";
 
-Vue.use(Antd);
+export default Antd;
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,3 +1,6 @@
-import "./antd";
+import Vue from "vue";
+import Antd from "./antd";
 import "./lodash";
 import "@/components";
+
+Vue.use(Antd);
