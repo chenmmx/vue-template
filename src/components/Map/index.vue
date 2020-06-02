634e59f517da6089bf44e224f118387efc7a2cac
ig666 <1249731680@qq.com>
2020-06-02 15:47:46 +0800

    modify map

--- src/components/Map/index.vue
+++ src/components/Map/index.vue
@@ -5,7 +5,14 @@
 <script>
 export default {
   name: "Map",
-  props: [""],
+  props: {
+    dataSource: {
+      type: Array
+    },
+    centerPoint: {
+      type: Object
+    }
+  },
   data() {
     return {
       trackAni: ""
@@ -27,54 +34,39 @@ export default {
     creatMap() {
       this.$nextTick(() => {
         var bmap = new window.BMapGL.Map("map"); // 创建Map实例
-        bmap.centerAndZoom(new window.BMapGL.Point(116.297611, 40.047363), 18); // 初始化地图，设置中心点坐标和地图级别
+        bmap.setMapStyleV2({
+          styleId: "5de6c8b1530e35878fedca88f99709e8"
+        });
+        var centerPoint = new window.BMapGL.Point(
+          this.centerPoint.x,
+          this.centerPoint.y
+        );
+        bmap.centerAndZoom(centerPoint, 19); // 初始化地图，设置中心点坐标和地图级别
+        var marker = new window.BMapGL.Marker(centerPoint); // 创建标注
+        bmap.addOverlay(marker);
+        var home = new window.BMapGL.Marker(
+          new window.BMapGL.Point(104.099786, 30.682027)
+        ); // 创建标注
+        bmap.addOverlay(home);
         bmap.enableScrollWheelZoom(true); // 开启鼠标滚轮缩放
-        var path = [
-          {
-            lng: 116.297611,
-            lat: 40.047363
-          },
-          {
-            lng: 116.302839,
-            lat: 40.048219
-          },
-          {
-            lng: 116.308301,
-            lat: 40.050566
-          },
-          {
-            lng: 116.305732,
-            lat: 40.054957
-          },
-          {
-            lng: 116.304754,
-            lat: 40.057953
-          },
-          {
-            lng: 116.306487,
-            lat: 40.058312
-          },
-          {
-            lng: 116.307223,
-            lat: 40.056379
-          }
-        ];
-        // bmap.setMapStyleV2({
-        //   styleId: '3d71dc5a4ce6222d3396801dee06622d'
-        // });
         var point = [];
-        for (var i = 0; i < path.length; i++) {
-          point.push(new window.BMapGL.Point(path[i].lng, path[i].lat));
+        for (var i = 0; i < this.dataSource.length; i++) {
+          point.push(
+            new window.BMapGL.Point(
+              this.dataSource[i].lng,
+              this.dataSource[i].lat
+            )
+          );
         }
         var pl = new window.BMapGL.Polyline(point, {
           strokeColor: "#2C8CEC",
           strokeWeight: 7,
-          strokeOpacity: 0.7
+          strokeOpacity: 0.5
         });
         this.trackAni = new window.BMapGLLib.TrackAnimation(bmap, pl, {
           overallView: true, // 动画完成后自动调整视野到总览
-          tilt: 30, // 轨迹播放的角度，默认为55
-          duration: 10000, // 动画持续时长，默认为10000，单位ms
+          tilt: 22, // 轨迹播放的角度，默认为55
+          duration: 20000, // 动画持续时长，默认为10000，单位ms
           delay: 3000 // 动画开始的延迟，默认0，单位ms
         });
       });
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -2,14 +2,55 @@
   <div class="home">
     <a-button type="primary" @click="start">开始</a-button>
     <div class="map">
-      <Map ref="map"></Map>
+      <Map ref="map" :dataSource="dataSource" :centerPoint="centerPoint"></Map>
     </div>
   </div>
 </template>
-
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      dataSource: [
+        {
+          lng: 104.099786,
+          lat: 30.682027
+        },
+        {
+          lng: 104.100639,
+          lat: 30.681612
+        },
+        {
+          lng: 104.099422,
+          lat: 30.679965
+        },
+        {
+          lng: 104.100711,
+          lat: 30.679189
+        },
+        {
+          lng: 104.10151,
+          lat: 30.678409
+        },
+        {
+          lng: 104.102049,
+          lat: 30.677127
+        },
+        {
+          lng: 104.100805,
+          lat: 30.676762
+        },
+        {
+          lng: 104.100953,
+          lat: 30.676277
+        }
+      ],
+      centerPoint: {
+        x: 104.10118,
+        y: 30.676088
+      }
+    };
+  },
   mounted() {
     console.log(123);
   },
@@ -27,7 +68,7 @@ export default {
   flex-direction: column;
   .map {
     width: 100%;
-    height: 38vw;
+    height: 80vh;
   }
 }
 </style>
