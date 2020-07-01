16165492afaeaf2a7a8dbd95bd717ad3f6b99418
chenmmx <1107204891@qq.com>
2020-07-01 16:54:19 +0800

    styles: col error

--- src/layout/components/NavBar/index.vue
+++ src/layout/components/NavBar/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="nav-bar-container">
     <a-row :gutter="15">
-      <a-col :xs="15" :md="24" :lg="12" :xl="12">
+      <a-col :xs="15" :sm="15" :md="12" :lg="12" :xl="12">
         <div class="left-panel">
           <!-- <a-icon
           v-show="device === 'desktop'"
@@ -13,7 +13,7 @@
           <tags-view></tags-view>
         </div>
       </a-col>
-      <a-col :xs="9" :sm="24" :md="12" :lg="12" :xl="12">
+      <a-col :xs="9" :sm="9" :md="12" :lg="12" :xl="12">
         <div class="right-panel">
           <!-- <global-search
           v-show="device === 'desktop'"
