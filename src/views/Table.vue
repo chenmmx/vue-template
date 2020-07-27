0523a480f44db3b81230c05a373758a28a291cda
陈淼鑫 <chenmiaoxin@chenmiaoxindeMacBook-Pro.local>
2020-07-27 15:34:20 +0800

    Merge branch 'pa'

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="table">
-    <public-header>test table</public-header>
+    <public-header>测试测试</public-header>
     <public-table
       :columns="columns"
       :data-source="data"
